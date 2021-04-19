b36dd8c02de033f19b230526486ac3319d5ff059
Ayush Tripathi <>
2021-04-19 15:00:55 +0000

    Just Wrote An Simple Program

---
+++ main.py
@@ -0,0 +1,9 @@
+# just writing some code for learning to connect and code with github and replit
+
+print("Welcome Dear User")
+try:
+ in1=str(input("Enter Your Name:"))
+ print(f"Welcome Master {in1}")
+except:
+  print("Please Enter Your Name Properly:")
+  
